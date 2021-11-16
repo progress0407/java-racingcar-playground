7c35fc7cedc1a6de0dcee19edb116e59e3a6f1e4
swcho <progress0407@gmail.com>
2021-11-16 23:04:25 +0900

    chore: 자동차 이름 초과 예외 처리
    자동차 이름은 5글자를 초과할 수 없음

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -5,7 +5,14 @@ public class Car {
     int move;
 
     public Car(String name, int move) {
+        validateName(name);
         this.name = name;
         this.move = move;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new RuntimeException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
     }
 }
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingCarTest {
 
@@ -16,5 +17,13 @@ public class RacingCarTest {
         Cars cars = new Cars("pobi,crong,honux");
         int carsCount = cars.getCars().size();
         assertThat(carsCount).isEqualTo(3);
+    }
+    
+    @Test
+    @DisplayName("자동차 이름은 5자 이상을 초과할 수 없습니다")
+    void name() {
+        assertThatThrownBy(() -> {
+            new Cars("pobi,crong,honuxx");
+        }).isInstanceOf(RuntimeException.class);
     }
 }
